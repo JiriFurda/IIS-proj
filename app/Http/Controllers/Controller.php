250b3a31b5d5c0aa7d8b95c690075e206f368902
Furdys <magiecz@gmail.com>
2018-12-01 15:51:03 +0100

    Reworked routes

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,16 +7,7 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
-/**
- *  Controller requiring user to be logged in order to view the page
- */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-	public function __construct()
-    {
-    	// Require user to be logged in
-        $this->middleware('auth');
-    }
 }
